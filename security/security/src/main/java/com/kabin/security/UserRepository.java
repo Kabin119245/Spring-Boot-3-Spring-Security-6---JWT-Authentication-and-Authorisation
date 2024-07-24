61632100a9af79764becaffe8220ac85a0e38a2f
Kabin119245 <kabin.077bct043@acem.edu.np>
2024-07-24 22:07:09 +0545

    JwtAuth filter and token

--- security/security/src/main/java/com/kabin/security/UserRepository.java
+++ security/security/src/main/java/com/kabin/security/UserRepository.java
@@ -3,6 +3,9 @@ package com.kabin.security;
 import com.kabin.security.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User,Integer> {
-    
+
+    Optional<User> findByEmail(String email);
 }
