e71694005b86c86829903e1ad705946da23ce90b
Kabin119245 <kabin.077bct043@acem.edu.np>
2024-07-24 21:52:01 +0545

    added model, repos

---
+++ security/security/src/main/java/com/kabin/security/UserRepository.java
@@ -0,0 +1,8 @@
+package com.kabin.security;
+
+import com.kabin.security.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Integer> {
+    
+}
---
+++ security/security/src/main/java/com/kabin/security/user/Role.java
@@ -0,0 +1,6 @@
+package com.kabin.security.user;
+
+public enum Role {
+
+    USER, ADMIN,
+}
